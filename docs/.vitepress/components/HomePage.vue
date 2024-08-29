944cdccf4111433e743b5774b3dd8c62ca53c600
XiaoMouz <gxiaomouz@gmail.com>
2024-08-29 17:16:04 +0800

    fix: mobile style, VPTeamMember no i18n

--- docs/.vitepress/components/HomePage.vue
+++ docs/.vitepress/components/HomePage.vue
@@ -1,52 +1,52 @@
 <script setup lang="ts">
-import { VPTeamMembers } from "vitepress/theme";
-import { computed } from "vue";
-import { useTranslate } from "../i18n/composable";
-import { iconMap } from "./icons";
+import { VPTeamMembers } from 'vitepress/theme'
+import { computed, shallowRef } from 'vue'
+import { useTranslate } from '../i18n/composable'
+import { iconMap } from './icons'
 
-const t = useTranslate();
+const t = useTranslate()
 
 const members = computed(() => [
   {
-    avatar: "https://github.com/tt702.png",
-    name: "TT702",
-    title: t("作者"),
+    avatar: 'https://github.com/tt702.png',
+    name: 'TT702',
+    title: t('作者'),
     links: [
-      { icon: "github", link: "https://github.com/TT702" },
+      { icon: 'github', link: 'https://github.com/TT702' },
       {
         icon: { svg: iconMap.bilibili },
-        link: "https://space.bilibili.com/515094",
+        link: 'https://space.bilibili.com/515094',
       },
     ],
   },
   {
-    avatar: "https://github.com/imfanhua.png",
-    name: "Fanhua",
-    title: t("团队成员"),
+    avatar: 'https://github.com/imfanhua.png',
+    name: 'Fanhua',
+    title: t('团队成员'),
     links: [
-      { icon: "github", link: "https://github.com/imfanhua" },
+      { icon: 'github', link: 'https://github.com/imfanhua' },
       {
         icon: { svg: iconMap.bilibili },
-        link: "https://space.bilibili.com/352711",
+        link: 'https://space.bilibili.com/352711',
       },
     ],
   },
   {
-    avatar: "https://github.com/ZhaiSoul.png",
-    name: "ZhaiSoul",
-    title: t("团队成员"),
-    links: [{ icon: "github", link: "https://github.com/ZhaiSoul" }],
+    avatar: 'https://github.com/ZhaiSoul.png',
+    name: 'ZhaiSoul',
+    title: t('团队成员'),
+    links: [{ icon: 'github', link: 'https://github.com/ZhaiSoul' }],
   },
-]);
+])
 </script>
 
 <template>
   <div flex="~ col wrap" mt8 items-center>
     <h2 text="center gray xl" mt-11 mb-4 pb-2>
-      {{ t("BakaXL 开发自 ❤") }}
+      {{ t('BakaXL 开发自 ❤') }}
     </h2>
-    <VPTeamMembers size="small" :members="members" />
-  </div>u
+    <VPTeamMembers class="w-full px-4" size="small" :members="members" />
+  </div>
 </template>
 
 <style></style>
--- docs/.vitepress/i18n/tokens.ts
+++ docs/.vitepress/i18n/tokens.ts
@@ -1,38 +1,38 @@
 export const eng = {
-  "BakaXL 文档中心": "BakaXL Help Center",
-  "提供 BakaXL 启动器在使用时疑难解答服务，同时也提供了一些其他的技术文档":
-    "Provides troubleshooting services for the BakaXL launcher when in use, as well as some other technical documents",
-  主页: "Home",
-  食用指南: "Start",
-  "遇到问题?": "FAQ",
-  "v3（稳定版）": "v3 (Stable)",
-  "v4（预览版）": "v4 (Preview)",
-  "用 💴 发电": "Make with 💴",
-  "在 GithuB 上编辑此页": "Edit this page on GitHub",
-  "zh-CN": "en-US",
-  中文: "English",
-  文档目前处在特殊状态: "The document is currently in a special state",
-  抢先体验: "Experience in advance",
-  过时文档: "Outdated documents",
-  封存文档: "Archived documents",
+  'BakaXL 文档中心': 'BakaXL Help Center',
+  '提供 BakaXL 启动器在使用时疑难解答服务，同时也提供了一些其他的技术文档':
+    'Provides troubleshooting services for the BakaXL launcher when in use, as well as some other technical documents',
+  主页: 'Home',
+  食用指南: 'Start',
+  '遇到问题?': 'FAQ',
+  'v3（稳定版）': 'v3 (Stable)',
+  'v4（预览版）': 'v4 (Preview)',
+  '用 💴 发电': 'Make with 💴',
+  '在 GithuB 上编辑此页': 'Edit this page on GitHub',
+  'zh-CN': 'en',
+  中文: 'English',
+  文档目前处在特殊状态: 'The document is currently in a special state',
+  抢先体验: 'Experience in advance',
+  过时文档: 'Outdated documents',
+  封存文档: 'Archived documents',
   本文档内功能处于早期阶段:
-    "The functions in this document are in the early stage",
-  "本文档部分内容已不再可靠，请等待更新":
-    "Some content in this document is no longer reliable, please wait for the update",
-  "本文档已被封存，仅供参考使用，文档所述均已失效或不可保证":
-    "This document has been archived and is for reference only. The content of the document is invalid or cannot be guaranteed",
-  "v3 相关文档": "v3 Related Documents",
-  游戏常见问题: "Common Game Issues",
-  游戏版本隔离: "Game Version Isolation",
-  制作游戏整合包: "Create Game Modpack",
-  编写规则: "Write Rules",
-  作者: "Author",
-  团队成员: "Team Member",
-  "BakaXL 开发自 ❤": "BakaXL Developed by ❤",
-  "关于在 Ubuntu 上运行 BakaXL 启动器的指引":
-    "Guide to running the BakaXL launcher on Ubuntu",
-};
+    'The functions in this document are in the early stage',
+  '本文档部分内容已不再可靠，请等待更新':
+    'Some content in this document is no longer reliable, please wait for the update',
+  '本文档已被封存，仅供参考使用，文档所述均已失效或不可保证':
+    'This document has been archived and is for reference only. The content of the document is invalid or cannot be guaranteed',
+  'v3 相关文档': 'v3 Related Documents',
+  游戏常见问题: 'Common Game Issues',
+  游戏版本隔离: 'Game Version Isolation',
+  制作游戏整合包: 'Create Game Modpack',
+  编写规则: 'Write Rules',
+  作者: 'Author',
+  团队成员: 'Team Member',
+  'BakaXL 开发自 ❤': 'BakaXL Developed by ❤',
+  '关于在 Ubuntu 上运行 BakaXL 启动器的指引':
+    'Guide to running the BakaXL launcher on Ubuntu',
+}
 
 export const langMap: Record<string, Record<string, string>> = {
   en: eng,
-};
+}
